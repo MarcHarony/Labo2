77fa912122d56aaaab85348c2f51a907fa7874c7
Nathan Vico <nathan.vico97@gmail.com>
2018-02-17 19:04:27 +0100

    Add column verification

--- labo2ex3.py
+++ labo2ex3.py
@@ -3,19 +3,29 @@ def verif(regexLignes, regexColonnes, solutions) :
     assert (len(regexLignes) == len(solutions))
     n=0
     m=0
+    o=0
+    q=0
+    r=0
     correct = True
     while n<len(solutions) :
         p = re.compile(regexLignes[n])
         if p.match(solutions[n]) is None :
-            correct = False
+            return False
         n+=1
-    print(correct)
-    solutionColonne = ['']
-    while m<len(solutions)-1 :
-        for solution in solutions :
-            solutionColonne[m] += solution[m]
+    solutionColonne = []
+    nbrColonnes = len(solutions[0])
+    while q<nbrColonnes :
+        solutionColonne.append('') #crée une liste avec le nombre de colonnes
+        q+=1
+    while m<len(solutions[0]) :
+        for solution in solutions : #pour chaque ligne
+            solutionColonne[m]+=solution[m] #ajouter l element d indice m a la colonne m
         m+=1
-    for truc in solutionColonne :
-        print(solutionColonne)
-verif (['[1-9][a-z]','[a-z][1-9]'],['\d','\d'],['5a', 'b9'])
-
+    while r<len(solutionColonne) :
+        p = re.compile(regexColonnes[r])
+        if p.match(solutionColonne[r]) is None :
+            return False
+        r+=1
+    return True
+print(verif (['[1-9][a-z][a-z]','[a-z][1-9][0-9]'],['[0-9][a-z]|[a-z][0-9]','[0-9][a-z]|[a-z][0-9]','[0-9][a-z]|[a-z][0-9]'],['5ab', 'b98']))
+print(verif (['[0-9]','[a-z]'],['[0-9][0-9]'],['5','a']))
